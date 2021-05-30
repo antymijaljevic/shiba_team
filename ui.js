f71d4c8ad4a8d9850473704e8050bc861864f52b
antymijlajevic <antymijaljevic@gmail.com>
2021-05-30 09:10:25 +0100

    thomas removed

--- ui.js
+++ ui.js
@@ -8,7 +8,6 @@ class UI{
             {Name: 'Husein', Amount: 100_000_000.00},
             {Name: 'Combo', Amount:  7_244_406.43},
             {Name: 'Luka', Amount:  24_884_798.0},
-            {Name: 'Tomas', Amount:  0},
         ]
 
         this.userCoins.sort((user1, user2) => user2.Amount - user1.Amount);
